4f3bc12833911ea4fb410a0afa00f64d9df4c98d
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 09:18:05 -0300

    redirecionamento para routes das entidades

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import clientsRouter from "./routes/client.route.js";
+import productsRouter from "./routes/product.route.js";
+import suppliersRouter from "./routes/supplier.route.js";
+import salesRouter from "./routes/sale.route.js";
 
 const app = express();
 
 //* Redirecionando todos as requisicoes para client.route
 app.use("/client", clientsRouter);
+app.use("/product", productsRouter);
+app.use("/supplier", suppliersRouter);
+app.use("/sale", salesRouter);
 
 app.listen(3000, () => {
     console.log("API Inicializada");
