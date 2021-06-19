eb27321395a40d9aa8dbc16d7f294d40438ce7aa
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 09:12:54 -0300

    redirecionamento para route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import clientsRouter from "./routes/client.route.js";
 
 const app = express();
+
+//* Redirecionando todos as requisicoes para client.rooute
+app.use("/client", clientsRouter);
 
 app.listen(3000, () => {
     console.log("API Inicializada");
