82bd4fbcd7a6164e868f2ac433e7be291b24d8a6
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 11:00:34 -0300

    codigo client.service

--- services/client.service.js
+++ services/client.service.js
@@ -1,6 +1,9 @@
 //? Service faz o tratamentoo da regra de negocio
+import ClientRepository from "../repositories/client.repository.js";
+
 async function createClient(client) {
-    
+    //* cria e retorna o objeto
+    return await ClientRepository.insertClient(client);
 }
 
 export default {
