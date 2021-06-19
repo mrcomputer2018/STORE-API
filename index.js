ab131fc5decf065293e1dffda50a67bd3443d398
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 09:23:42 -0300

    cors

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 //* para express usar json
 app.use(express.json());
 
+app.use(cors());
+
 //* Redirecionando todos as requisicoes para client.route
 app.use("/client", clientsRouter);
 app.use("/product", productsRouter);
