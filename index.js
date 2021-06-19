230c2b2f3cd7abde162c0a1fab10f0f9f767c7d4
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 09:33:46 -0300

    coonfig winston

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ import productsRouter from "./routes/product.route.js";
 import suppliersRouter from "./routes/supplier.route.js";
 import salesRouter from "./routes/sale.route.js";
 
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({
+    level,
+    message,
+    label,
+    timestamp
+}) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+//* Colocando winston como global
+//* Transport - para imprimir no console e no arquivo texto
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: "my-bank-api.log"})
+    ],
+    format: combine(
+        label({ label: "my-bank-api"}),
+        timestamp(),
+        myFormat
+    )
+});
+
 const app = express();
 
 //* para express usar json
