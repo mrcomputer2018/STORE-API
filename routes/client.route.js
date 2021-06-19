f8769af692eeb552c6c63c73020de89d27ff8bf9
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-19 10:33:07 -0300

    post ppara createClient

--- routes/client.route.js
+++ routes/client.route.js
@@ -1,9 +1,12 @@
 //? Routesr envia as requisicoes para controller
-
 import express from "express";
+import clientController from "../controllers/client.controller.js";
 
 //* Criando o roteador para receber as requisicoes
 const router = express.Router();
+
+//* Rota POST
+router.post("/", clientController.createClient);
 
 //* Exportando o roteador
 export default router;
