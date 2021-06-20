6335ce7a2aa59d929a9c5ee7f03429879b176edd
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-20 06:46:11 -0300

    codigo client.repository

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,7 +1,16 @@
-import pg from "pg";
+import { connect } from "./db.js"
 
 async function insertClient(client) {
+    //* Fazendo a conexao
+    const conn = await connect();
 
+    //* Montando o SQL que executaremos
+    const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5)";
+    //* Evitando o ataque de sql injection
+    const values = [client.name, client.cpf, client.phoone, client.email, client.address];
+
+    const res = await conn.query(sql,values);
+    return {};
 }
 
 export default {
