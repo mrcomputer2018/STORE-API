e3c9fb9b97d138888226c87e40396a72f4961eab
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-20 06:16:12 -0300

    codigo de conexao com o BD

---
+++ repositories/db.js
@@ -0,0 +1,19 @@
+import pg from "pg";
+
+async function connect() {
+    if (global.connection) {
+        return global.connection.connect();
+    }
+    //* Criando o pool para agilizar a conexao com o banco
+    const pool = new pg.Pool({
+       connectionString: "postgres://mgkwxtyu:TAKB4CEoG5--tuAxliHoM7wL0-Ja1ZW7@batyr.db.elephantsql.com/mgkwxtyu"
+    })
+    //* Colocando como global para nao criar varios pools
+    // * a palicacao ficar lenta
+    global.connection = pool;
+    return pool.connect();
+}
+
+export default {
+    connect
+};
