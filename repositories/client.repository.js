af04e0f5e9d2f52e65cdd966ec7be6f0951dcf74
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-22 14:34:03 -0300

    codigo de updateClient em repository

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -10,7 +10,7 @@ async function insertClient(client) {
         const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
         //* Evitando o ataque de sql injection
         const values = [client.name, client.cpf, client.phone, client.email, client.address];
-
+        //* Guardadndo em res o objeto alterado
         const res = await conn.query(sql, values);
 
         return res.rows[0];
@@ -85,9 +85,27 @@ async function deleteClient(id) {
     }
 } 
 
+async function updateClient() {
+    const conn = await connect();
+
+    try {
+        const sql = "UPDATE clients SET name = $1, cpf = $2, phone = $3, email = $4, address = $5 WHERE client_id $6 RETURN *";
+        const values = [client.name, client.cpf, client.phone, client.email, client.address, client.client_id];
+        const res = await conn.query(sql, values);
+
+        return res.rows[0];
+
+    } catch (err) {
+        throw err;
+
+    } finally {
+        conn.release();    }
+}
+
 export default {
     insertClient,
     getClients,
     getClient,
-    deleteClient   
+    deleteClient, 
+    updateClient 
 }
