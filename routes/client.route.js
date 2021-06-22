44e4194d3cefa0606eda283dc057d63894bc480f
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-22 14:13:37 -0300

    adicionando comentario

--- routes/client.route.js
+++ routes/client.route.js
@@ -5,14 +5,14 @@ import ClientController from "../controllers/client.controller.js";
 //* Criando o roteador para receber as requisicoes
 const router = express.Router();
 
-//* Rota POST
+//* Rota POST passando a funcao como parametro
 router.post("/", ClientController.createClient);
 
-//* Rota GET
+//* Rota GET  passando a funcao como parametro
 router.get("/", ClientController.getClients);
 router.get("/:id", ClientController.getClient);
 
-//* Rota DELETE
+//* Rota DELETE passando a funcao como parametro
 router.delete("/:id", ClientController.deleteClient);
 
 //* Exportando o roteador
