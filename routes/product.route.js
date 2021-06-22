ef797b52fe6cc6b078d8cf2b145cc2efcc9f9613
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-22 18:20:05 -0300

    codigo pproductRoutes

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,5 +1,22 @@
+//? Routesr envia as requisicoes para controller
 import express from "express";
+import ProductController from "../controllers/product.controller.js";
 
+//* Criando o roteador para receber as requisicoes
 const router = express.Router();
 
+//* Rota POST passando a funcao como parametro
+router.post("/", ProductController.createProduct);
+
+//* Rota GET  passando a funcao como parametro
+router.get("/", ProductController.getProducts);
+router.get("/:id", ProductController.getProduct);
+
+//* Rota DELETE passando a funcao como parametro
+router.delete("/:id", ProductController.deleteProduct);
+
+//* Rota PUT
+router.put("/", ProductController.updateProduct);
+
+//* Exportando o roteador
 export default router;
