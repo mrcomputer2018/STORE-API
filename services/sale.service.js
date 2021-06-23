e9b3d2d0144128678c9be12ae1e6d93d456d2726
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-23 10:10:26 -0300

    validacao em updateSale

--- services/sale.service.js
+++ services/sale.service.js
@@ -10,8 +10,10 @@ async function createSale(sale) {
     if(!await ClientRepository.getClient(sale.client_id)) {
         errors.push("O Client_id informado não existe");
     }
+    //*consumindo banco de dados uma vez somente
+    const product = await ProductRepository.getProduct(sale.product_id);
 
-    if (!await ProductRepository.getProduct(sale.product_id)) {
+    if (!product) {
         errors.push("O Product_id informado nao existe");
     }
     //* Se tiver error
@@ -19,6 +21,9 @@ async function createSale(sale) {
        //* Mandando para index.js o erro
        throw errors;
     }
+
+    //* Validacao de estoque
+    const product = ProductRepository.getProduct(sale.product_id);
     
     //* cria e retorna o objeto
     return await SaleRepository.insertSale(sale);
@@ -38,10 +43,27 @@ async function deleteSale(id) {
 }
 
 async function updateSale(sale) {
-    if(await SupplierReposytory.getSupplier(sale.supplier_id)) {
-        return await SaleRepository.updateSale(sale);
+    const errors = [];
+
+    if (!await SaleRepository.getSale(sale.sale_id)) {
+        errors.push("O Sale_id informado não existe");
     }
-    throw new Error("O supplier_id informado não existe");
+    //* Se nao existir o Client_id
+    if(!await ClientRepository.getClient(sale.client_id)) {
+        errors.push("O Client_id informado não existe");
+    }
+
+    if (!await ProductRepository.getProduct(sale.product_id)) {
+        errors.push("O Product_id informado nao existe");
+    }
+    //* Se tiver error
+   if (errors.length !== 0) {
+       //* Mandando para index.js o erro
+       throw errors;
+    }
+
+    return await SaleRepository.updateSale(sale);
+   
 }
 
 export default {
