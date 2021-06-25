cf98c696069935033b9a7441f109ded069493c9e
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-25 18:23:23 -0300

    codigo de updateClient com sequelize

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -41,20 +41,17 @@ async function deleteClient(id) {
 } 
 
 async function updateClient(client) {
-    const conn = await connect();
-
     try {
-        const sql = "UPDATE clients SET name = $1, cpf = $2, phone = $3, email = $4, address = $5 WHERE client_id = $6 RETURNING *";
-        const values = [client.name, client.cpf, client.phone, client.email, client.address, client.client_id];
-        const res = await conn.query(sql, values);
-
-        return res.rows[0];
-
+       await Client.update(client, {
+           where: {
+               clientId: client.clientId
+           }
+       });
+       return await getClient(client.clientId);
+       
     } catch (err) {
         throw err;
-
-    } finally {
-        conn.release();    }
+    }
 }
 
 export default {
