baa7718c47213396e6bb6500a98b1b663589c1a7
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-25 15:03:56 -0300

    Inserindo sequelize no DB

--- repositories/db.js
+++ repositories/db.js
@@ -1,19 +1,13 @@
-import pg from "pg";
+import Sequelize, { Sequelize } from "sequelize";
 
-async function connect() {
-    if (global.connection) {
-        return global.connection.connect();
+const sequelize = new Sequelize(
+    "postgres://mgkwxtyu:TAKB4CEoG5--tuAxliHoM7wL0-Ja1ZW7@batyr.db.elephantsql.com/mgkwxtyu",
+    {
+        dialect: "postgres",
+        define: {
+            timestamps: false
+        }
     }
-    //* Criando o pool para agilizar a conexao com o banco
-    const pool = new pg.Pool({
-       connectionString: "postgres://mgkwxtyu:TAKB4CEoG5--tuAxliHoM7wL0-Ja1ZW7@batyr.db.elephantsql.com/mgkwxtyu"
-    })
-    //* Colocando como global para nao criar varios pools
-    // * a aplicacao nao ficar lenta
-    global.connection = pool;
-    return pool.connect();
-}
+);
 
-export {
-    connect
-};
+export default sequelize;
