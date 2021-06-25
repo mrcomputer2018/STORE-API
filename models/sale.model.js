031af271b40cb67f5da57a64e3a01cc098106831
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-25 16:17:16 -0300

    modelando sale

---
+++ models/sale.model.js
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+import db from "../repositories/db.js";
+//* importando supplier para usar no belongsTo
+import Supplier from "./supplier.model.js";
+import Client from "../models/client.model.js";
+
+//* Defimindo a classe
+const Sale = db.define('sales', {
+    saleId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        promaryKey: true
+    },
+    value: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    date: {
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+}, { underscored : true });
+
+//* Criando a associacao para incluir o objeto de supplierID
+//* belongsTo - ele pertence a alguem
+Product.belongsTo(Client, { foreignKey: "clientId"});
+Product.belongsTo(Supplier, { foreignKey: "supplierId"});
+
+//* Exportando classe
+export default Sale;
