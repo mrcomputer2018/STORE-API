9d6ad985e2da678b72e788197dc83317a99f1112
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-27 07:23:56 -0300

    codigo de supplier com sequelize

--- repositories/supplier.repository.js
+++ repositories/supplier.repository.js
@@ -1,106 +1,55 @@
-import connect from "./db.js"
+//* Importando Modelo
+import Supplier from "../models/supplier.model.js";
 
 async function insertSupplier(supplier) {
-    //* Fazendo a conexao
-    const conn = await connect();
-
     try {
-        //* Montando o SQL que executaremos
-        //* Returning para retornar o que eu quero
-        const sql = "INSERT INTO suppliers (name, cnpj, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-        //* Evitando o ataque de sql injection
-        const values = [supplier.name, supplier.cnpj, supplier.phone, supplier.email, supplier.address];
-        //* Guardadndo em res o objeto alterado
-        const res = await conn.query(sql, values);
-
-        return res.rows[0];
-
+        return await Supplier.create(supplier);
     } catch (err) {
-        throw err; //* sera jogado para frente e tratado na rota que criamos
-    } finally {
-        //* Com finally a conexao sempre esra fechada
-        //* Realizando o release da conexao
-        conn.release();
+        throw err;
     }
 }
 
-//* Metodo buscar fornecedores
+//* Metodo buscar supplieres
 async function getSuppliers () {
-    const conn = await connect();
-
     try {
-        const sql = "SELECT * FROM suppliers";
-        
-        const res = await conn.query(sql);
-        //* pegando toda a lista de fornecedores
-        return res.rows;
-
+        return await Supplier.findAll();
     } catch (err) {
-       
         throw err;
-
-    } finally {
-
-        conn.release();
     }
 }
 
 async function getSupplier(id) {
-    const conn = await connect();
-
     try {
-        const sql = "SELECT * FROM suppliers WHERE supplier_id = $1";
-
-        const values = [id];
-
-        const res = await conn.query(sql, values);
-
-         return res.rows[0];
-
+        //* acha pela PK
+        return await Supplier.findByPk(id);
     } catch (err) {
         throw err;
-
-    } finally {
-        conn.release();
-
     }
 }
 
 async function deleteSupplier(id) {
-    const conn = await connect();
-
     try {
-        const sql = "DELETE FROM suppliers WHERE supplier_id = $1";
-
-        const values = [id];
-
-        await conn.query(sql, values);
-        //* nao coloca RES pois nao esta esperando nenhum retorno
-
+        await Supplier.destroy({
+            where: {
+                supplierId: id
+            }
+        });
     } catch (err) {
         throw err;
-
-    } finally {
-        conn.release();
     }
 } 
 
 async function updateSupplier(supplier) {
-    const conn = await connect();
-
     try {
-        const sql = "UPDATE suppliers SET name = $1, cnpj = $2, phone = $3, email = $4, address = $5 WHERE supplier_id = $6 RETURNING *";
-        const values = [supplier.name, supplier.cnpj, supplier.phone, supplier.email, supplier.address, supplier.supplier_id];
-        const res = await conn.query(sql, values);
-
-        return res.rows[0];
+       await Supplier.update(supplier, {
+           where: {
+               supplierId: supplier.supplierId
+           }
+       });
+       return await getSupplier(supplier.supplierId);
 
     } catch (err) {
         throw err;
-
-    } finally {
-        conn.release();
-    
     }
 }
 
