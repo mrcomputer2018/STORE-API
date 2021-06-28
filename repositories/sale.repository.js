d5575747a045b5fb6edff42b7a40e19e3055f6a5
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-06-28 13:18:20 -0300

    codigo para seqielize updateSale

--- repositories/sale.repository.js
+++ repositories/sale.repository.js
@@ -91,22 +91,28 @@ async function deleteSale(id) {
 } 
 
 async function updateSale(sale) {
-    const conn = await connect();
-
+   
     try {
-        const sql = "UPDATE sales SET value = $1, date = $2, client_id = $3, product_id = $4 WHERE sale_id = $5 RETURNING *";
-        const values = [sale.value, sale.date, sale.client_id, sale.product_id, sale.sale_id];
-        const res = await conn.query(sql, values);
+        //* Nao alterar SaleId para evitar inconsistencias
+        await Sale.update({
+                //* campos que quero atualizar
+                value: sale.value,
+                date: sale.date,
+                clientId: sale.clientId
+            }, 
+            {
+            where: {
+               saleId: sale.saleId
+            }
+        });
 
-        return res.rows[0];
+        //* Retornando a venda atualizada pro usuario
+        return await getSale(sale.saleId)
 
     } catch (err) {
         throw err;
 
-    } finally {
-        conn.release();
-    
-    }
+    } 
 }
 
 export default {
