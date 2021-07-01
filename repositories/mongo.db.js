b12c2c4856bf4d3f3c24a4a874a71f19c853c42d
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-07-01 09:15:19 -0300

    codigo de mongodb.js com mongoose

--- repositories/mongo.db.js
+++ repositories/mongo.db.js
@@ -1,8 +1,8 @@
-import mongodb from "mongodb";
+import mongoose from "mongoose";
 
-function getClient() {
+async function connect() {
     const uri = "mongodb+srv://proeja:m0411n0304@cluster0.g01er.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    return new mongodb.MongoClient(uri);
+    return await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 }
 
-export { getClient };
+export { connect };
